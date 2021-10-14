fcb33bbe14e655a5d76efd4c4f813ef6a6c44e66
BradLi <Brad@163.com>
2021-10-15 01:37:19 +0800

    Third IDEA COMMIT

--- src/main/java/com/atguigu/git/Gtest.java
+++ src/main/java/com/atguigu/git/Gtest.java
@@ -12,5 +12,6 @@ public class Gtest {
     public static void main(String[] args) {
         System.out.println("hello GGGGG");
         System.out.println("hello iiii");
+        System.out.println("hello tttt");
     }
 }
