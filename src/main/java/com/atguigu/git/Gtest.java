3d1a6f40323e86fdf9aeffe19035dd8259c7ca49
BradLi <Brad@163.com>
2021-10-15 01:35:23 +0800

    FIRST IDEA COMMIT

---
+++ src/main/java/com/atguigu/git/Gtest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;/**
+ * @author shkstart
+ * @create 2021-10-15 1:30
+ */
+
+/**
+ *@Description TODO
+ *@Author VanDark
+ *@Date 2021/10/151:30
+ */
+public class Gtest {
+    public static void main(String[] args) {
+        System.out.println("hello GGGGG");
+    }
+}
