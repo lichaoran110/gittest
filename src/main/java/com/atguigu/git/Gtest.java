c26211d8b57b8f0bccb1954e0aaaa7e7c266991d
BradLi <Brad@163.com>
2021-10-15 20:50:59 +0800

    MM2 IDEA COMMIT

--- src/main/java/com/atguigu/git/Gtest.java
+++ src/main/java/com/atguigu/git/Gtest.java
@@ -13,6 +13,7 @@ public class Gtest {
         System.out.println("hello GGGGG");
         System.out.println("hello iiii");
         System.out.println("hello tttt");
+        System.out.println("hello MasterTest");
 
 
     }
